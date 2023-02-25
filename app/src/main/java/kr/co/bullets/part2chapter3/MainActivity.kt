3d14afa3ef53b81028d916e5271b434d3952f95f
Bullets-jyr <jyr@bullets.co.kr>
2023-02-25 17:10:24 +0900

    05. OKHttp 를 이용하여 Client 앱 만들기

--- app/src/main/java/kr/co/bullets/part2chapter3/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part2chapter3/MainActivity.kt
@@ -2,10 +2,40 @@ package kr.co.bullets.part2chapter3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val client = OkHttpClient()
+
+        val request: Request = Request.Builder()
+            .url("http://192.168.0.103:8080")
+            .build()
+
+        val callback = object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("Client", e.toString())
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    Log.e("Client", "${response.body?.string()}")
+                }
+            }
+        }
+
+        client.newCall(request).enqueue(callback)
+
+//        try {
+//            val response = client.newCall(request).execute()
+//            Log.e("Client", "${response.body?.string()}")
+//        } catch (e: Exception) {
+//            Log.e("Client", e.toString())
+//        }
     }
 }
