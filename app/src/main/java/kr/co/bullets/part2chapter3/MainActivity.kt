cc333175620b47a1860c92c4bb7447c2d9d61adb
Bullets-jyr <jyr@bullets.co.kr>
2023-02-25 18:51:11 +0900

    06. 클라이언트 앱 UI 그리기

--- app/src/main/java/kr/co/bullets/part2chapter3/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part2chapter3/MainActivity.kt
@@ -3,6 +3,13 @@ package kr.co.bullets.part2chapter3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import okhttp3.*
 import java.io.IOException
 
@@ -11,25 +18,54 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val editText = findViewById<EditText>(R.id.serverHostEditText)
+        val confirmButton = findViewById<Button>(R.id.confirmButton)
+        val informationTextView = findViewById<TextView>(R.id.informationTextView)
         val client = OkHttpClient()
+        var serverHost = ""
 
-        val request: Request = Request.Builder()
-            .url("http://192.168.0.103:8080")
-            .build()
-
-        val callback = object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                Log.e("Client", e.toString())
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                if (response.isSuccessful) {
-                    Log.e("Client", "${response.body?.string()}")
-                }
-            }
+        editText.addTextChangedListener {
+            serverHost = it.toString()
         }
 
-        client.newCall(request).enqueue(callback)
+        confirmButton.setOnClickListener {
+            val request: Request = Request.Builder()
+//                .url("http://192.168.0.103:8080")
+                .url("http://$serverHost:8080")
+                .build()
+
+            val callback = object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.e("Client", e.toString())
+                    runOnUiThread {
+                        Toast.makeText(this@MainActivity, "수신에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    if (response.isSuccessful) {
+//                        Log.e("Client", "${response.body?.string()}")
+                        val response = response.body?.string()
+
+                        runOnUiThread {
+                            informationTextView.isVisible = true
+                            informationTextView.text = response
+
+                            editText.isVisible = false
+                            confirmButton.isVisible = false
+                        }
+                    } else {
+                        runOnUiThread {
+                            Toast.makeText(this@MainActivity, "수신에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+
+            client.newCall(request).enqueue(callback)
+        }
+
+//        client.newCall(request).enqueue(callback)
 
 //        try {
 //            val response = client.newCall(request).execute()
